4c49ad4ef55fecd3eba4ad5386451933fa16dd77
Pedro <pedroge2828@gmail.com>
2016-09-30 18:31:10 +0200

    ejercicio01

--- ejercicio01.php
+++ ejercicio01.php
@@ -2,12 +2,7 @@
 
 $array = array();
 
-array_push($array,"uno");
-array_push($array,"dos");
-array_push($array,"tres");
-array_push($array,"cuatro");
-array_push($array,"cinco");
-
+array_push($array,"uno","dos","tres","cuatro","cinco");
 
 foreach ($array as $element){ 
     echo $element . '<br>'; 
